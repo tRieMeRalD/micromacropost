1303d8c923dc9e985262dabea345b0e00177e078
unknown <horizon.william14@gmail.com>
2019-09-04 17:03:21 -0400

    Initial commit

---
+++ server/routes/api/posts.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const router = express.Router();
+
+// Gets posts
+router.get("/", async (req, res) => {
+  // Retrieve post collection to work with
+  const posts = await loadPostsCollection();
+
+  // Get all posts
+  res.send(await posts.find({}).toArray());
+});
+
+// Add posts
+router.post("/", async (req, res) => {
+  const posts = await loadPostsCollection();
+
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+
+  // 201 = Status OK + sent some info
+  res.status(201).send();
+});
+
+// Delete
+router.delete("/:id", async (req, res) => {
+  const posts = await loadPostsCollection();
+
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
+});
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    "mongodb+srv://abc123:qwerty1234@cluster0-dcuu9.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
+
+  return client.db("vuejs").collection("posts");
+}
+
+module.exports = router;
